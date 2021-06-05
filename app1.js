a5ea33a01bcc4e2daef9945ab42e0bac3bd2470e
Andreas <asc4asc@users.noreply.github.com>
2021-06-05 14:24:09 +0200

    Update app1.js

--- app1.js
+++ app1.js
@@ -5,7 +5,7 @@ let tmp=document.querySelector("body");
 let out=document.querySelector("out");
 
 tmp.style.backgroundColor = "orange";
-var noSleep = new ns.NoSleep();
+// var noSleep = new ns.NoSleep();
 
 let i = 1;
 let i1 = 4;
